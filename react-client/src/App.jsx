bace78439fb5df2827fa205a965af0f96b5f84a4
Daniel Bae <danibae91@icloud.com>
2022-11-03 12:45:52 -0700

    work on react version of carousel

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,8 +1,116 @@
-import "./App.css"; 
+import { useState } from "react"; 
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@mui/icons-material"; 
+import { sliderItems } from "./constants";
+import styled from "styled-components"; 
+
+const Container = styled.div`
+    width: 100%; 
+    height: 100%; 
+    display: flex; 
+    overflow: hidden; 
+    position: relative; 
+`
+
+const Arrow = styled.div`
+    width: 50px; 
+    height: 50px; 
+    background-color: #fff7f7; 
+    border-radius: 50%; 
+    display: flex; 
+    justify-content: center; 
+    align-items: center; 
+    position: absolute; 
+    top: 0; 
+    bottom: 0; 
+    left: ${props=> props.direction === "left" && "10px"};
+    right: ${props=> props.direction === "right" && "10px"}; 
+    margin: auto; 
+    cursor: pointer; 
+    opacity: 0.5; 
+    z-index: 2; 
+`
+
+const Wrapper = styled.div`
+    height: 100%; 
+    display: flex; 
+    transition: all 1.5s ease; 
+    transform: translateX(${props=>props.slideIndex * -100}vw); 
+`
+
+const Slide = styled.div`
+    width: 100vw; 
+    height: 50vh; 
+    display: flex; 
+    align-items: center; 
+    background-color: #${props=>props.bg}; 
+`
+const ImgContainer = styled.div`
+    flex: 1; 
+    width: 100%; 
+    height: 100%; 
+`
+
+const Image = styled.img`
+    height: 100%; 
+    width: 50vw; 
+    object-fit: cover; 
+`
+const InfoContainer = styled.div`
+    flex: 1; 
+    display: flex; 
+    justify-content: center; 
+    align-items: flex-start; 
+    flex-direction: column; 
+    margin-left: 25px; 
+`
+
+const Title = styled.h1`
+    font-size: 20px; 
+`
+const Subtitle = styled.h3`
+    font-size: 18px; 
+`
+const Desc = styled.p`
+    margin: 50px 0px; 
+    font-size: 16px; 
+    font-weight: 500; 
+    letter-spacing: 3px; 
+`
 
 const App = () => {
+    const [slideIndex, setSlideIndex] = useState(0); 
+
+    const handleClick = (direction) => {
+        if(direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length-1);
+        } else {
+            setSlideIndex(slideIndex < sliderItems.length-1 ? slideIndex  + 1 : 0);
+        };
+    };
+
     return (
-        <h1>Hello World</h1>
+        <Container>
+            <Arrow direction="left" onClick={()=> handleClick("left")}>
+                <ArrowBackIosOutlined />
+            </Arrow>
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map((item) => (
+                    <Slide key={item.id} bg={item.bg}>
+                        <ImgContainer>
+                            <Image src={item.img} alt="img" />
+                        </ImgContainer>
+                        <InfoContainer>
+                            <Title>{item.title}</Title>
+                            <Subtitle>{item.subtitle}</Subtitle>
+                            <Desc>{item.desc}</Desc>
+                        </InfoContainer>
+                    </Slide>
+                ))}
+            </Wrapper>
+            <Arrow direction="right" onClick={()=> handleClick("right")}>
+                <ArrowForwardIosOutlined />
+            </Arrow>
+        </Container>
     )
 }
 
---
+++ react-client/src/constants.js
@@ -0,0 +1,228 @@
+import walton1 from "./assets/walton1.jpg"; 
+import walton2 from "./assets/walton2.jpg"; 
+import walton3 from "./assets/walton3.jpg"; 
+import walton4 from "./assets/walton4.jpg"; 
+import walton5 from "./assets/walton5.jpg"; 
+import walton6 from "./assets/walton6.jpg"; 
+import walton7 from "./assets/walton7.jpg"; 
+import walton8 from "./assets/walton8.jpg"; 
+import walton9 from "./assets/walton9.jpg"; 
+import walton10 from "./assets/walton10.jpg"; 
+import walton11 from "./assets/walton11.jpg"; 
+import walton12 from "./assets/walton12.jpg"; 
+import walton13 from "./assets/walton13.jpg"; 
+import walton14 from "./assets/walton14.jpg"; 
+import walton15 from "./assets/walton15.jpg"; 
+import walton16 from "./assets/walton16.jpg"; 
+import walton17 from "./assets/walton17.jpg"; 
+import walton18 from "./assets/walton18.jpg"; 
+import walton19 from "./assets/walton19.jpg"; 
+import walton20 from "./assets/walton20.jpg"; 
+import walton21 from "./assets/walton21.jpg"; 
+import walton22 from "./assets/walton22.jpg"; 
+import walton23 from "./assets/walton23.jpg"; 
+import walton24 from "./assets/walton24.jpg"; 
+import walton25 from "./assets/walton25.jpg"; 
+import walton26 from "./assets/walton26.jpg"; 
+import walton27 from "./assets/walton27.jpg"; 
+import walton28 from "./assets/walton28.jpg"; 
+
+export const sliderItems = [
+    {
+        id: 1, 
+        img: walton1, 
+        title: "2238 W Walton St, #3", 
+        subtitle: "Chicago, IL 60622", 
+        desc: "@Ukrainian Village",
+        bg: "f5fafd",
+    }, 
+    {
+        id: 2, 
+        img: walton2, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3, 
+        img: walton3, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "fbf0f4",
+    },
+    {
+        id: 4, 
+        img: walton4, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 5, 
+        img: walton5, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f5fafd",
+    },
+    {
+        id: 6, 
+        img: walton6, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 7, 
+        img: walton7, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 8, 
+        img: walton8, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 9, 
+        img: walton9, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f5fafd",
+    },
+    {
+        id: 10, 
+        img: walton10, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 11, 
+        img: walton11, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 12, 
+        img: walton12, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 13, 
+        img: walton13, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f5fafd",
+    },
+    {
+        id: 14, 
+        img: walton14, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 15, 
+        img: walton15, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 16, 
+        img: walton16, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 17, 
+        img: walton17, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f5fafd",
+    },
+    {
+        id: 18, 
+        img: walton18, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 19, 
+        img: walton19, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 20, 
+        img: walton20, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 21, 
+        img: walton21, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 22, 
+        img: walton22, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 23, 
+        img: walton23, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 24, 
+        img: walton24, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 25, 
+        img: walton25, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 26, 
+        img: walton26, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 27, 
+        img: walton27, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+    {
+        id: 28, 
+        img: walton28, 
+        title: "", 
+        desc: "@Ukrainian Village",
+        bg: "f7agg3",
+    },
+];
